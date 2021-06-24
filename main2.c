c35971b7b0d585ea89683be0109a2f57d84fddc2
Yash Kamal Chaturvedi <69413160+yashkamalchaturvedi@users.noreply.github.com>
2021-06-24 19:23:24 +0530

    4th commit
    main2 updated

--- main2.c
+++ main2.c
@@ -1,32 +1,30 @@
 #include <stdio.h>
+// includes string functions
 #include <string.h>
 
-int glo = 76;
+// Function removes the repeating same code many time
+// Syntax of Function
 // data_type func_name(parameter_type parametername){
 //     return value_of_type_data_type;
 // }
 
-void newPrint(char * char1)
-{
-    printf("The value is %s\n\n\n\n", char1);
-}
-
+// Function to add 2 numbers
 int sum(int a, int b)
 {
-    return a+b;
+    return a + b;
 }
-
 float average(float a, float b)
 {
-    return (a+b)/2;
+    return (a + b) / 2;
 }
 
-struct Books{
+// A structure is a user defined data type in which we can create a data type that can be used to group items of possibly different types into a single type.
+struct Books
+{
     char name[50];
     char author[50];
     int price;
 } book;
-
 
 void printStruct(struct Books bk)
 {
@@ -34,51 +32,42 @@ void printStruct(struct Books bk)
     printf("Author name is %s\n", bk.author);
     printf("Book Price is %d\n", bk.price);
 }
-int main(){
 
-    
+int main()
+{
 
-    // Functions
-    // printf("The average of 100 and 299 is %f", average(100,299));
-    // int glo = 87;
-    // printf("Glo is %d", glo);
-    // int arr[10] = {0,1,2,3,4,5,6,70,8,9};
-    // for(int i = 0; i < 10; i++)
-    // {
-    //     printf("Enter the values for index %d \n", i);
-    //     scanf("%d", &arr[i]);
-    // }
+    // An array is defined as the collection of similar type of data items
+    int arr[10] = {0, 1, 2, 3, 4, 5, 6, 70, 8, 9};
+    for (int i = 0; i < 10; i++)
+    {
+        printf("Enter the values for index %d \n", i);
+        scanf("%d", &arr[i]);
+    }
+    for (int i = 0; i < 10; i++)
+    {
+        printf("The value for index %d is %d\n", i, arr[i]);
+    }
+    printf("%d", arr[7]);
 
-    // for(int i = 0; i < 10; i++)
-    // {
-    //     printf("The value for index %d is %d\n", i, arr[i]);
-    // }
-    
-    
-    // printf("%d", arr[7]);
+    // Pointers is a variable which stores the address of another variable
+    int a = 98;
+    printf("%d\n", a);
+    int *ptr = NULL; // Nothing in it
+    if (!ptr)
+    {
+        printf("pointer is not null");
+    }
+    ptr = &a;   // ptr stores address of a
+    *ptr = 188; // change the value to which pointer is pointing
+    printf("%d", a);
 
-    // Pointers
-    // int a = 98;
-    // printf("%d\n", a);
-    // int* ptr = NULL;
-    // if (!ptr)
-    // {
-    //     printf("pointer is not null");
-    // }
-    // ptr = &a;
-    // *ptr = 188;
-    // printf("%d", a);
-
-
-    // Strings
-    // char name[3] = {'m', 'y', '\0'};
-    // char str1[65], str2[98], str3[32];
-    // strcpy(str1, "Harry");
-    // strcpy(str2, "Rohan");
-    // strcat(str1, str2);
-    // printf("%d", strcmp(str1,str2));
-
-    
+    // Strings is a sequence of characters terminated with a null character \0 .
+    char name[3] = {'m', 'y', '\0'};
+    char str1[65], str2[98], str3[32];
+    strcpy(str1, "Ash");
+    strcpy(str2, "Ron");
+    strcat(str1, str2);
+    printf("%d", strcmp(str1, str2));
 
     // Structures
     struct Books bk1, bk2;
@@ -88,6 +77,4 @@ int main(){
     printStruct(bk1);
 
     return 0;
-
-    
 }
